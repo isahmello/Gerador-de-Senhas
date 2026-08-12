82942b25b6dedb694a0c5294ebb84c0e64e3a1ce
isahmello <isadora.mello.santos@escola.pr.gov.br>
2026-08-12 16:07:56 -0300

    2

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 const numeroSenha= document.querySelector('.parametro-_texto');
-numeroSenha.textContent=5;
-const botoes=document.querySelector(parametro-senha_botao);
+let tamanhoSenha=12;//variavel que pode ser alterada
+
+numeroSenha.textContent=tamanhoSenha;//atribui o valor do "tamanhoSenha" ao "numeroSenha"
+const botoes=document.querySelector(parametro-senha_botao);//selecionando todas as classse "parametro-senha_botao"
+
+botoes[0].onclick=diminuiTamanho;
+function diminuiTamanho() {
+tamanhoSenha=tamanhoSenha-1;//diminui de 1 o valor do "tamanhoSenha"
+numeroSenha.textContent=tamanhoSenha;//atribui o valor do "tamanhoSenha" ao "numeroSenha"
+}
+
+
+botoes[1].onclick=aumentaTamanho;
+function aumentaTamanho() {
+tamanhoSenha=tamanhoSenha+1;//aumenta de 1 o valor do "tamanhoSenha"
+numeroSenha.textContent=tamanhoSenha;//atribui o valor do "tamanhoSenha" ao "numeroSenha"
+}
