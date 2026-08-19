0fbcea48e59c209aaf78ed68c330dc615f8a254a
isahmello <isadora.mello.santos@escola.pr.gov.br>
2026-08-19 16:23:50 -0300

    12

--- main.js
+++ main.js
@@ -4,20 +4,37 @@ let tamanhoSenha=12;//variavel que pode ser alterada
 numeroSenha.textContent=tamanhoSenha;//atribui o valor do "tamanhoSenha" ao "numeroSenha"
 const botoes=document.querySelectorAll(parametro-senha_botao);//selecionando todas as classse "parametro-senha_botao"
 
-botoes[0].onclick=diminuiTamanho;
+botoes[0].onclick=diminuiTamanho();
+botoes[1].onclick=aumentaTamanho();
+
 function diminuiTamanho() {
     if(tamanhoSenha<1){
 tamanhoSenha=tamanhoSenha-1;//diminui de 1 o valor do "tamanhoSenha"     
+tamanhoSenha--;
 }
 numeroSenha.textContent=tamanhoSenha;//atribui o valor do "tamanhoSenha" ao "numeroSenha"
 }
 
-
-botoes[1].onclick=aumentaTamanho;
 function aumentaTamanho() {
     if (tamanhoSenha>20){
- tamanhoSenha=tamanhoSenha+1;//aumenta de 1 o valor do "tamanhoSenha"       
+ //tamanhoSenha=tamanhoSenha+1;//aumenta de 1 o valor do "tamanhoSenha"    
+ tamanhoSenha++;
     }
 
 numeroSenha.textContent=tamanhoSenha;//atribui o valor do "tamanhoSenha" ao "numeroSenha"
 }
+
+const campoSenha=document.querySelector(#campo-senha);
+const letrasMaiusculas='ABCDEFG';
+geraSenha();
+function geraSenha(){
+
+    for (let i = 0;i < tamanhoSenha; i++) {
+        
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio=Math.floor(numeroAleatorio);
+console.log(letrasMaiusculas[numeroAleatorio]);
+
+}
+}
+campoSenha.value = letrasMaiusculas;
