dcca5f04415fd44eaa6538644d6a8ecf7d9abfb5
isahmello <isadora.mello.santos@escola.pr.gov.br>
2026-09-02 15:43:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -31,15 +31,13 @@ geraSenha();
 const campoSenha=document.querySelector(#campo-senha);
 const letrasMaiusculas='ABCDEFG';
 
-geraSenha();
-function geraSenha(){
-let senha ="";
 
-    for (let i = 0;i < tamanhoSenha; i++) {    
-let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-numeroAleatorio=Math.floor(numeroAleatorio);
-console.log(letrasMaiusculas[numeroAleatorio]);
-senha=senha+letrasMaiusculas[numeroAleatorio];
+function geraSenha(){
+let senha = '';
+for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
 }
+campoSenha.value = senha;
 }
-campoSenha.value =senha;
